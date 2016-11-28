2e26549214214297304e5f25c7ff270719ec3f68
Anna <annakatherinebaker@gmail.com>
2016-11-28 13:29:26 -0600

    some more db

--- server/modules/connection.js
+++ server/modules/connection.js
@@ -1,4 +1,5 @@
-var connectionString = 'postgres://localhost:5432/primerDB';
+var connectionString =     'postgres://pzycofpzsxekhq:U8BIrxQyWUoagknLPqmYgSs4_5@ec2-54-243-200-159.compute-1.amazonaws.com:5432/d5eo9mo48rhsrt';
+
 
 if(process.env.DATABASE_URL !== undefined) {
     connectionString = process.env.DATABASE_URL + 'ssl';
